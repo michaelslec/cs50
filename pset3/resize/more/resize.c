e4239ca4882f04b733a57517257a51dac94f8fc1
Michael LeCompte <michaelslec98@gmail.com>
2019-11-27 23:05:58 -0800

    Formatting to match style50 specifications

--- pset3/resize/more/resize.c
+++ pset3/resize/more/resize.c
@@ -6,29 +6,32 @@
 
 #include "bmp.h"
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     // ensure proper usage
-    if (argc != 4) {
+    if (argc != 4)
+    {
         fprintf(stderr, "Usage: copy f infile outfile\n");
         return 1;
     }
 
     // remember filenames & int
     float res_factor = atof(argv[1]);
-    char* infile = argv[2];
-    char* outfile = argv[3];
+    char *infile = argv[2];
+    char *outfile = argv[3];
 
     // open input file
-    FILE* inptr = fopen(infile, "r");
-    if (inptr == NULL) {
+    FILE *inptr = fopen(infile, "r");
+    if (inptr == NULL)
+    {
         fprintf(stderr, "Could not open %s.\n", infile);
         return 2;
     }
 
     // open output file
-    FILE* outptr = fopen(outfile, "w");
-    if (outptr == NULL) {
+    FILE *outptr = fopen(outfile, "w");
+    if (outptr == NULL)
+    {
         fclose(inptr);
         fprintf(stderr, "Could not create %s.\n", outfile);
         return 3;
@@ -43,7 +46,8 @@ int main(int argc, char* argv[])
     fread(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
 
     // ensure infile is (likely) a 24-bit uncompressed BMP 4.0
-    if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 || bi.biBitCount != 24 || bi.biCompression != 0) {
+    if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 || bi.biBitCount != 24 || bi.biCompression != 0) 
+    {
         fclose(outptr);
         fclose(inptr);
         fprintf(stderr, "Unsupported file format.\n");
@@ -64,11 +68,13 @@ int main(int argc, char* argv[])
     // determine padding for scanlines
     int padding = (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
 
-    // recalculate size variables
-    bi.biSizeImage = ((sizeof(RGBTRIPLE) * bi.biWidth) + padding)
-        * abs(bi.biHeight);
-    bf.bfSize = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER)
-        + bi.biSizeImage;
+    // recalculate header size information
+    bi.biSizeImage = (sizeof(RGBTRIPLE) * bi.biWidth + padding) 
+                     * abs(bi.biHeight);
+
+    bf.bfSize = sizeof(BITMAPFILEHEADER) 
+                + sizeof(BITMAPINFOHEADER) 
+                + bi.biSizeImage;
 
     // write outfile's BITMAPFILEHEADER
     fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
@@ -83,29 +89,25 @@ int main(int argc, char* argv[])
     // iterate through new file scanlines
     // NOTE: biHeight is negative
     printf("bi.biHeight: %d", abs(bi.biHeight));
-    for (int i = 0; i < abs(bi.biHeight); i++) {
+    for (int i = 0; i < abs(bi.biHeight); i++)
+    {
         // iterate through RGB bytes
-        for (int j = 0; j < bi.biWidth; j++) {
+        for (int j = 0; j < bi.biWidth; j++)
+        {
             // temporary RGB storate
             RGBTRIPLE temp;
 
             // ratio multiplier for nearest neighbor
             px = j * x_ratio;
             py = i * y_ratio;
-            neighbor_coefficient
-                // size of steps      nearest neighbor algorithm
-                = sizeof(RGBTRIPLE) * (py * orig_width + px)
-                // account for padding
-                + (orig_padding * py);
+            neighbor_coefficient = sizeof(RGBTRIPLE) 
+                                   * (py * orig_width + px) 
+                                   * + (orig_padding * py);
 
             // location of nearest neighbor in original file
-            int nearest_neighbor
-                // skip file header
-                = sizeof(BITMAPFILEHEADER)
-                // skip info header
-                + sizeof(BITMAPINFOHEADER)
-                // seek to pixel according to coefficient
-                + neighbor_coefficient;
+            int nearest_neighbor = sizeof(BITMAPFILEHEADER)
+                                   + sizeof(BITMAPINFOHEADER)
+                                   + neighbor_coefficient;
 
             // navigate to nearest neighbor in original file
             fseek(inptr, nearest_neighbor, SEEK_SET);
@@ -117,34 +119,10 @@ int main(int argc, char* argv[])
 
         // then add it back to the output file (to demonstrate how)
         for (int k = 0; k < padding; k++)
+        {
             fputc(0x00, outptr);
+        }
     }
-
-    //
-    // OLD COPYING AGLORITHM
-    //
-    /* // iterate over infile's scanlines */
-    /* for (int i = 0, biHeight = abs(bi.biHeight); i < biHeight; i++) { */
-    /*     // iterate over pixels in scanline */
-    /*     for (int j = 0; j < bi.biWidth; j++) { */
-    /*         // temporary storage */
-    /*         RGBTRIPLE triple; */
-
-    /*         // read RGB triple from infile */
-    /*         fread(&triple, sizeof(RGBTRIPLE), 1, inptr); */
-
-    /*         // write RGB triple to outfile */
-    /*         fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr); */
-    /*     } */
-
-    /*     // skip over padding, if any */
-    /*     fseek(inptr, orig_padding, SEEK_CUR); */
-
-    /*     // then add it back to the output file (to demonstrate how) */
-    /*     for (int k = 0; k < padding; k++) { */
-    /*         fputc(0x00, outptr); */
-    /*     } */
-    /* } */
 
     // close infile
     fclose(inptr);
