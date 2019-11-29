d4c34a3f6ca7018a28f31ba4cedbdaffd8cf61ac
Michael LeCompte <michaelslec98@gmail.com>
2019-11-29 15:01:35 -0600

    scan through file and fine jpgs

--- pset3/recover/recover.c
+++ pset3/recover/recover.c
@@ -19,5 +19,26 @@ int main(int argc, char *argv[])
         return 2;
     }
 
+    // char didn't work, so short it is
+    const size_t  kbuffer_size = 512;
+    size_t        nth_jpeg     = 0;
+    unsigned char buffer[kbuffer_size];
+
+    // eof < 512, so read until can't read 512
+    while (fread(buffer, sizeof(char), kbuffer_size, inptr) == kbuffer_size)
+    {
+        // check for start of jpg
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff
+            && (buffer[3] & 0xf0) == 0xe0)
+        {
+            ++nth_jpeg;
+        }
+    }
+
+    printf("%zd JPG's found\n", nth_jpeg);
+
+    // close file to recover
+    fclose(inptr);
+
     return 0;
 }
