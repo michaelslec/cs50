d6064ac07e9a7e3d6bfd4e66ae1688be6d669615
Michael LeCompte <michaelslec98@gmail.com>
2019-11-29 15:57:49 -0600

    finished

--- pset3/recover/recover.c
+++ pset3/recover/recover.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -21,7 +22,10 @@ int main(int argc, char *argv[])
 
     const size_t  kbuffer_size = 512; // bytes in a block of FAT memory
     size_t        nth_jpg      = 0; // num of jpg's found
-    unsigned char buffer[kbuffer_size];
+    unsigned char buffer[kbuffer_size]; // buffer for one block of FAT memory
+    char          filename[8]; // storage for filename 00x.jpg + \0
+    bool          file_open = 0; // is a jpg already open?
+    FILE *        img;
 
     // eof < 512, so read until can't read 512
     while (fread(buffer, sizeof(char), kbuffer_size, inptr) == kbuffer_size)
@@ -30,14 +34,37 @@ int main(int argc, char *argv[])
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff
             && (buffer[3] & 0xf0) == 0xe0)
         {
-            ++nth_jpg;
+            // if a file is already open && there's a new jpg header
+            if (file_open)
+            {
+                fclose(img);
+                ++nth_jpg;
+                sprintf(filename, "%03zd.jpg", nth_jpg);
+                img = fopen(filename, "w");
+                fwrite(buffer, kbuffer_size, 1, img);
+            }
+            // first jpg found
+            else
+            {
+                file_open = 1;
+                ++nth_jpg;
+                sprintf(filename, "%03zd.jpg", nth_jpg);
+                img = fopen(filename, "w");
+                fwrite(buffer, kbuffer_size, 1, img);
+            }
+        }
+        // file is already open, and its not the start of another jpg
+        else if (file_open)
+        {
+            fwrite(buffer, kbuffer_size, 1, img);
         }
     }
 
-    printf("%zd JPG's found\n", nth_jpg);
-
     // close file to recover
     fclose(inptr);
+
+    // close image file
+    fclose(img);
 
     return 0;
 }
