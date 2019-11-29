58881bb5d8c2fffb7f80f7abbc0191b89e94dfcc
Michael LeCompte <michaelslec98@gmail.com>
2019-11-29 02:26:05 -0600

    Added argument checking

--- pset3/recover/recover.c
+++ pset3/recover/recover.c
@@ -5,7 +5,8 @@ int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        printf("Usage: ./recover file.jpeg\n");
+        fprintf(stderr, "Usage: ./recover file.jpeg\n");
+        return 1;
     }
 
     return 0;
