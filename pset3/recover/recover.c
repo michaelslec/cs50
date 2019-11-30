15b8fbde0c839112a2d28b60cc134c56b935a803
Michael LeCompte <michaelslec98@gmail.com>
2019-11-30 00:46:33 -0600

    Refactored code to not operated like garbage

--- pset3/recover/recover.c
+++ pset3/recover/recover.c
@@ -1,6 +1,9 @@
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
+
+// essentially unsigned char
+typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
@@ -20,41 +23,32 @@ int main(int argc, char *argv[])
         return 2;
     }
 
-    const size_t  kbuffer_size = 512; // bytes in a block of FAT memory
-    size_t        nth_jpg      = 0; // num of jpg's found
-    unsigned char buffer[kbuffer_size]; // buffer for one block of FAT memory
-    char          filename[8]; // storage for filename 00x.jpg + \0
-    bool          file_open = 0; // is a jpg already open?
-    FILE *        img;
+    const size_t kbuffer_size = 512; // bytes in a block of FAT memory
+    size_t       image_count = 0; // num of jpg's found
+    BYTE         buffer[kbuffer_size]; // buffer for one block of FAT memory
+    char         filename[8]; // storage for filename 00x.jpg + \0
+    FILE *       img = NULL;
 
     // eof < 512, so read until can't read 512
-    while (fread(buffer, sizeof(char), kbuffer_size, inptr) == kbuffer_size)
+    while (fread(buffer, sizeof(BYTE), kbuffer_size, inptr) == kbuffer_size)
     {
-        // check for start of jpg
-        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff
-            && (buffer[3] & 0xf0) == 0xe0)
+        bool jpg_header_found = buffer[0] == 0xff && buffer[1] == 0xd8
+                                && buffer[2] == 0xff
+                                && (buffer[3] & 0xf0) == 0xe0;
+
+        if (jpg_header_found && img != NULL)
         {
-            // if a file is already open && there's a new jpg header
-            if (file_open)
-            {
-                fclose(img);
-                sprintf(filename, "%03zd.jpg", nth_jpg);
-                img = fopen(filename, "w");
-                fwrite(buffer, kbuffer_size, 1, img);
-                ++nth_jpg;
-            }
-            // first jpg found
-            else
-            {
-                file_open = 1;
-                sprintf(filename, "%03zd.jpg", nth_jpg);
-                img = fopen(filename, "w");
-                fwrite(buffer, kbuffer_size, 1, img);
-                ++nth_jpg;
-            }
+            fclose(img);
+            ++image_count;
         }
-        // file is already open, and its not the start of another jpg
-        else if (file_open)
+
+        if (jpg_header_found)
+        {
+            sprintf(filename, "%03zd.jpg", image_count);
+            img = fopen(filename, "w");
+        }
+
+        if (img != NULL)
         {
             fwrite(buffer, kbuffer_size, 1, img);
         }
