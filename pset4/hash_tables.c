6bb95042c855b7eaf432e7b3f85a8a2581ca9b56
Michael LeCompte <michaelslec98@gmail.com>
2019-11-30 03:19:58 -0600

    hash table that works

--- pset4/hash_tables.c
+++ pset4/hash_tables.c
@@ -1,79 +1,125 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-typedef struct node
+#define ALPHABET_SIZE 26
+
+enum alphabet
 {
-    int          n;
-    struct node *next;
-} node;
+    a,
+    b,
+    c,
+    d,
+    e,
+    f,
+    g,
+    h,
+    i,
+    j,
+    k,
+    l,
+    m,
+    n,
+    o,
+    p,
+    q,
+    r,
+    s,
+    t,
+    u,
+    v,
+    w,
+    x,
+    y,
+    z
+};
+
+typedef struct name_list
+{
+    char *            n;
+    struct name_list *next;
+} name_list;
 
 int main(void)
 {
-    node *numbers = NULL;
+    name_list *names[ALPHABET_SIZE] = { 0 };
 
     while (true)
     {
         // get input
-        int number = get_int("Number: ");
+        string name = get_string("name: ");
+        size_t first_letter = tolower(name[0]) - 'a';
 
-        // allows ctrl-d to exit while loop
-        if (number == INT_MAX)
+        // no input breaks loop
+        if (!strcmp(name, ""))
         {
             break;
         }
 
-        // temporary storage for nesxt node
-        node *n = malloc(sizeof(node));
+        // temporary storage for nesxt name_list
+        name_list *na = malloc(sizeof(name_list));
         // check if malloc succeed
-        if (!n)
+        if (!na)
         {
+            fprintf(stderr, "could not allocate memory sizeof %ld",
+                    sizeof(name_list));
             return 1;
         }
 
         // update values for temporary storage
-        n->n = number;
-        n->next = NULL;
+        na->n = name;
+        na->next = NULL;
 
         // already has elements
-        if (numbers)
+        if (names[first_letter])
         {
-            // iterate through nodes
-            for (node *ptr = numbers; ptr != NULL; ptr = ptr->next)
+            // iterate through name_lists
+            for (name_list *ptr = names[first_letter]; ptr != NULL;
+                 ptr = ptr->next)
             {
-                // one last node is found
+                // one last name_list is found
                 if (!ptr->next)
                 {
-                    // set last node next value to temporary node
-                    ptr->next = n;
+                    // set last name_list next value to temporary name_list
+                    ptr->next = na;
                     break;
                 }
             }
         }
         else // first element
         {
-            // set first node
-            numbers = n;
+            // set first name_list
+            names[first_letter] = na;
         }
     }
 
-    // print each node value, and next node address
-    for (node *ptr = numbers; ptr != NULL; ptr = ptr->next)
+    // print each name_list value, and next name_list address
+    for (int alph = 0; alph < ALPHABET_SIZE; ++alph)
     {
-        printf("value: %d, next address %p\n", ptr->n, (void *)ptr->next);
+        printf("letter %c\n", alph + 'a');
+        for (name_list *ptr = names[alph]; ptr != NULL; ptr = ptr->next)
+        {
+            printf("    value: %s, next address %p\n", ptr->n,
+                   (void *)ptr->next);
+        }
     }
 
-    // create an node iterator for numbers
-    node *ptr = numbers;
     // while ptr points to real memory
-    while (ptr)
+    for (size_t alph = 0; alph < ALPHABET_SIZE; ++alph)
     {
-        // store next node value
-        node *next = ptr->next;
-        // free current memory
-        free(ptr);
-        // set up for next loop
-        ptr = next;
+        // create an name_list iterator for numbers
+        name_list *ptr = names[alph];
+        while (ptr)
+        {
+            // store next name_list value
+            name_list *next = ptr->next;
+            // free current memory
+            free(ptr);
+            // set up for next loop
+            ptr = next;
+        }
     }
 
     return 0;
