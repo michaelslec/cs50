598d5f03289694583f8e2a18e03c569a12b51e46
Michael LeCompte <michaelslec98@gmail.com>
2019-12-11 01:38:47 -0800

    formatting

--- pset4/speller/dictionary.h
+++ pset4/speller/dictionary.h
@@ -10,9 +10,9 @@
 #define LENGTH 45
 
 // Prototypes
-bool load(const char *dictionary);
+bool         load(const char *dictionary);
 unsigned int size(void);
-bool check(const char *word);
-bool unload(void);
+bool         check(const char *word);
+bool         unload(void);
 
 #endif // DICTIONARY_H
